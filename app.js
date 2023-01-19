0c3fbc84f35a54a2ea6aef558a1913a9bbba52df
Jibbril Berbres <jibbril.berbres@viego.se>
2023-01-19 07:57:22 +0100

    Zip files to csv and save complete

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const JSZip = require('jszip');
 const { saveAs } = require('file-saver');
 const fs = require('fs');
+const papa = require('papaparse');
+
+
+const data = [
+    [{
+        "col1": "val1",
+        "col2": "val2",
+        "col3": "val3",
+        "col4": "val4",
+    },
+    {
+        "col1": "val1",
+        "col2": "val2",
+        "col3": "val3",
+        "col4": "val4",
+    }],
+    [{
+        "col1": "val1",
+        "col2": "val2",
+        "col3": "val3",
+        "col4": "val4",
+    }]
+]
+
+const aCsvs = data.map(arr => {
+    return papa.unparse(arr)
+});
 
 const zip = new JSZip();
-zip.folder("test").file("test.txt", "hejhej");
+
+aCsvs.forEach((csv, i) => {
+    zip.file(`test${i}.csv`, csv);
+});
+
 
 // zip.generateAsync({type:"base64"}).then(content => {
 //     saveAs(content, "test.zip");
